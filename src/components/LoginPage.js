ca330f56eb1e9915267df78945838e4ce88c152f
ncrmro <ncrmro@gmail.com>
2018-04-28 12:53:36 -0500

    Add flow example and tighten up settings

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react'
+// @flow
+import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { graphql } from 'react-apollo'
-import  { gql } from 'apollo-boost'
+import { gql } from 'apollo-boost'
 import { AUTH_TOKEN } from '../constant'
 
-class LoginPage extends Component {
+type Props = {
+  refreshTokenFn: Function,
+  history: {
+    replace: Function
+  },
+  loginMutation: Function
+};
+
+type State = {
+  email: string,
+  password: string
+}
+
+class LoginPage extends React.Component<Props, State> {
   state = {
     email: '',
     password: '',
-  }
+  };
 
   render() {
     return (
-      <div className="pa4 flex justify-center bg-white">
+      <div className="pa4 flex justify-center bg-white" >
         <div>
           <h3>
-            Don't have an account? <a href="/signup">Signup</a>
+            Don't have an account? <a href="/signup" >Signup</a>
           </h3>
           <input
             autoFocus
@@ -34,14 +48,14 @@ class LoginPage extends Component {
             value={this.state.password}
           />
           {this.state.email &&
-            this.state.password && (
-              <button
-                className="pa3 bg-black-10 bn dim ttu pointer"
-                onClick={this._login}
-              >
-                Log in
-              </button>
-            )}
+          this.state.password && (
+            <button
+              className="pa3 bg-black-10 bn dim ttu pointer"
+              onClick={this._login}
+            >
+              Log in
+            </button>
+          )}
         </div>
       </div>
     )
@@ -60,16 +74,16 @@ class LoginPage extends Component {
         const token = result.data.login.token
 
         this.props.refreshTokenFn &&
-          this.props.refreshTokenFn({
-            [AUTH_TOKEN]: token,
-          })
+        this.props.refreshTokenFn({
+          [AUTH_TOKEN]: token,
+        })
         this.props.history.replace('/')
         window.location.reload()
       })
       .catch(err => {
         console.log('error')
       })
-  }
+  };
 }
 
 const LOGIN_USER_MUTATION = gql`
