5205a867a01224b2110d13e0ef46ebe53bb22ccb
Nicholas Romero <ncrmro@gmail.com>
2018-07-16 21:25:36 -0500

    feature(tools): working initial ssr

--- client/src/shared/components/Html.tsx
+++ client/src/shared/components/Html.tsx
@@ -2,17 +2,18 @@ import * as React from "react";
 import { ApolloProvider } from "react-apollo";
 import Routes from "shared/components/Routes";
 
-function html(props: { content: any, apolloClient: any }) {
-  return (
-    <html>
+class Html extends React.Component<{content: any, apolloClient: any}, {}> {
+  public render():JSX.Element {
+    const {content} = this.props
+    return<html>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </head>
       <body>
-        <div id="content" dangerouslySetInnerHTML={{ __html: props.content }} />
+        <div id="content" dangerouslySetInnerHTML={{ __html: content }}/>
       </body>
-    </html>
-  );
+    </html>;
+  }
 }
 
-export default html;
+export default Html;
