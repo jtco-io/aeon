469f82972613ab41cbb2bbc619a496d55bfa8675
Nicholas Romero <ncrmro@gmail.com>
2018-07-31 10:00:39 -0500

    refactor(env): dry the urls and infer from http, host and port settings rather than URL's

--- client/src/config/env.ts
+++ client/src/config/env.ts
@@ -1,43 +1,57 @@
-declare const __PRODUCTION__: boolean;
-declare const __PROJECT_TITLE__: string;
-declare const __GRAPHQL_URL__: string;
-declare const __FRONTEND_PORT__: number;
-declare const __FRONTEND_HOST__: string;
-declare const __BACKEND_PORT__: number;
-declare const __BACKEND_HOST__: string;
+declare const __PRODUCTION__:boolean;
+declare const __PROJECT_TITLE__:string;
+declare const __FRONTEND_PORT__:number;
+declare const __FRONTEND_HOST__:string;
+declare const __BACKEND_PORT__:number;
+declare const __BACKEND_HOST__:string;
+declare const __HTTPS__:boolean;
 
+interface Backend {
+  graphql: {
+    host: string;
+    port: number;
+    directory: string;
+  };
+}
+interface Frontend {
+  host: string;
+  port: number;
+}
 export interface Env {
   PRODUCTION: boolean;
   PROJECT_TITLE: string;
+  FRONTEND_URL: string;
   GRAPHQL_URL: string;
-  backend: {
-    graphql: {
-      host: string;
-      port: number;
-      directory: string;
-    };
-  };
-  frontend: {
-    host: string;
-    port: number;
-  };
+  backend: Backend;
+  frontend: Frontend;
+  HTTPS: boolean
 }
 
-export const env: Env = {
+
+const frontend = {
+  host: __FRONTEND_HOST__||"localhost",
+  port: __FRONTEND_PORT__||8080,
+}
+
+
+const backend = {
+  graphql: {
+    host: __BACKEND_HOST__||"localhost",
+    port: __BACKEND_PORT__||8081,
+    directory: "graphql",
+  }
+}
+
+const isHTTPS = __HTTPS__ ? 'https' : 'http'
+
+export const env:Env = {
+  frontend,
+  backend,
+  HTTPS: __HTTPS__,
   PRODUCTION: __PRODUCTION__,
   PROJECT_TITLE: __PROJECT_TITLE__,
-  GRAPHQL_URL: __GRAPHQL_URL__ || "http://localhost:8081/graphql",
-  frontend: {
-    host: __FRONTEND_HOST__ || "localhost",
-    port: __FRONTEND_PORT__ || 8080,
-  },
-  backend: {
-    graphql: {
-      host: __BACKEND_HOST__ || "localhost",
-      port: __BACKEND_PORT__ || 8081,
-      directory: "graphql",
-    },
-  },
+  FRONTEND_URL: `${isHTTPS}://${frontend.host}:${frontend.port}`,
+  GRAPHQL_URL: `${isHTTPS}://${backend.graphql.host}:${backend.graphql.port}`,
 };
 
 export default env;
--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -16,51 +16,67 @@ declare const process: {
     ["FRONTEND_PORT"]: number;
     ["BACKEND_HOST"]: string;
     ["BACKEND_PORT"]: number;
+    ["HTTPS"]: boolean;
   };
 };
 const {
   NODE_ENV,
   PROJECT_TITLE,
-  GRAPHQL_URL,
   FRONTEND_HOST,
   FRONTEND_PORT,
   BACKEND_HOST,
   BACKEND_PORT,
+  HTTPS,
 } = process.env;
+
+
+interface Backend {
+  graphql: {
+    host: string;
+    port: number;
+    directory: string;
+  };
+}
+interface Frontend {
+  host: string;
+  port: number;
+}
 
 export interface Env {
   NODE_ENV: string;
   PRODUCTION: boolean;
   PROJECT_TITLE: string;
+  FRONTEND_URL: string;
   GRAPHQL_URL: string;
-  backend: {
-    graphql: {
-      host: string;
-      port: number;
-      directory: string;
-    };
-  };
-  frontend: {
-    host: string;
-    port: number;
-  };
+  backend: Backend;
+  frontend: Frontend;
+  HTTPS: boolean,
 }
+
+const frontend = {
+  host: FRONTEND_HOST||"localhost",
+  port: FRONTEND_PORT||8080,
+}
+
+
+const backend = {
+  graphql: {
+    host: BACKEND_HOST||"localhost",
+    port: BACKEND_PORT||8081,
+    directory: "graphql",
+  }
+}
+
+const isHTTPS = HTTPS ? 'https' : 'http'
 
 export const env: Env = {
   NODE_ENV,
   PROJECT_TITLE,
+  frontend,
+  backend,
+  HTTPS: HTTPS,
   PRODUCTION: NODE_ENV === "production",
-  GRAPHQL_URL: GRAPHQL_URL || "http://localhost:8081/graphql",
-  frontend: {
-    host: FRONTEND_HOST || "localhost",
-    port: FRONTEND_PORT || 8080,
-  },
-  backend: {
-    graphql: {
-      host: BACKEND_HOST || "localhost",
-      port: BACKEND_PORT || 8081,
-      directory: "graphql",
-    },
-  },
+  FRONTEND_URL: `${isHTTPS}://${frontend.host}:${frontend.port}`,
+  GRAPHQL_URL: `${isHTTPS}://${backend.graphql.host}:${backend.graphql.port}`,
 };
 export default env;
