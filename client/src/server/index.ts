1374c44837e3bf06d58527e3dd8e62f84e796dfc
Nicholas Romero <ncrmro@gmail.com>
2018-07-31 22:56:03 -0500

    feat(cleint): convert frontend server to use typescript

--- client/src/server/index.ts
+++ client/src/server/index.ts
@@ -1,67 +1,61 @@
-const {resolve, join} = require('path')
+const { resolve, join } = require("path");
 
-const
-  clientServerDir = resolve(__dirname),
+const clientServerDir = resolve(__dirname),
   clientSrcDir = resolve(clientServerDir, ".."),
   clientDir = resolve(clientSrcDir, ".."),
   clientBuildDir = resolve(clientDir, "build"),
   projRoot = resolve(clientDir, ".."),
-  envFile = resolve(projRoot, ".env")
+  envFile = resolve(projRoot, ".env");
 
-require('dotenv').config({path: envFile})
+require("dotenv").config({ path: envFile });
 
-const
-  port = process.env.FRONTEND_PORT || 8080,
-  PROD = process.env.NODE_ENV === "production"
+const port = process.env.FRONTEND_PORT || 8080,
+  PROD = process.env.NODE_ENV === "production";
 
-const
-  fallback = require('express-history-api-fallback'),
+const fallback = require("express-history-api-fallback"),
   express = require("express"),
-  app = express()
+  app = express();
 
 if (!PROD) {
-  const proxy = require('http-proxy-middleware');
+  const proxy = require("http-proxy-middleware");
 
-  function isSW(pathname, req) {
-    const isSW = /^\Wsw\.js/.test(pathname)
-    return isSW ? true : false
+  function isSW(pathname: string) {
+    const isSW = /^\Wsw\.js/.test(pathname);
+    return isSW ? true : false;
   }
 
-  app.use(proxy(isSW, { target: 'http://localhost:6080/static/bundles/' }))
+  app.use(proxy(isSW, { target: "http://localhost:6080/static/bundles/" }));
 
-  const
-    webpack = require("webpack"),
+  const webpack = require("webpack"),
     webpackDevMiddleware = require("webpack-dev-middleware"),
     webpackHotMiddleware = require("webpack-hot-middleware"),
     webpackHotServerMiddleware = require("webpack-hot-server-middleware"),
-    config = require("../config/webpack.js")
+    config = require("../config/webpack.js");
 
   const compiler = webpack(config);
 
   app.use(
     webpackDevMiddleware(compiler, {
       serverSideRender: true,
-      publicPath: '/static',
-      //stats: 'minimal'
-    })
+      publicPath: "/static",
+      // stats: 'minimal'
+    }),
   );
   app.use(
     webpackHotMiddleware(
-      compiler.compilers.find(compiler => compiler.name === "client")
-    )
+      compiler.compilers.find((compiler: any) => compiler.name === "client"),
+    ),
   );
   app.use(webpackHotServerMiddleware(compiler));
 } else {
-  const
-    CLIENT_ASSETS_DIR = join(clientBuildDir, "./client"),
+  const CLIENT_ASSETS_DIR = join(clientBuildDir, "./client"),
     CLIENT_STATS_PATH = join(CLIENT_ASSETS_DIR, "stats.json"),
     SERVER_RENDERER_PATH = join(clientBuildDir, "./server");
-  const
-    serverRenderer = require(SERVER_RENDERER_PATH).default,
+  const serverRenderer = require(SERVER_RENDERER_PATH).default,
     stats = require(CLIENT_STATS_PATH);
   app.use(express.static(CLIENT_ASSETS_DIR));
   app.use(serverRenderer(stats));
 }
 
-app.use(fallback('index.html'))
+app.use(fallback("index.html"));
 app.listen(port, () => console.log(`Express now listening on port ${port}!`));
