236bb84d8d88dbd32e7704818d17a38dc9599a65
Nicholas Romero <ncrmro@gmail.com>
2018-08-13 21:40:25 -0500

    feat(client): make loadableApp

--- client/src/screens/App/index.ts
+++ client/src/screens/App/index.ts
@@ -1,14 +1,18 @@
 import * as ReactRouter from "react-router";
 import * as Loadable from "react-loadable";
+import Loading from "shared/components/Loading";
+
+// @ts-ignore
+const loadableApp = Loadable({
+  loader: () =>
+    import(/* webpackChunkName: "app" */ "screens/App/screens/Home/screens/Landing"),
+  loading: Loading,
+});
 
 const app: ReactRouter.RouteProps[] = [
   {
     path: "/",
-    component: Loadable({
-      loader: () =>
-        import(/* webpackChunkName: "app" */ "screens/App/screens/Home/screens/Landing"),
-      loading: (): any => null,
-    }),
+    component: loadableApp,
   },
 ];
 
