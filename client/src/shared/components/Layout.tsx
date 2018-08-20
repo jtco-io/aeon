fc348e2896e7cff1cd038a08459e3b6837b74629
Nicholas Romero <ncrmro@gmail.com>
2018-08-19 23:37:35 -0500

    fix(client): add initial theme context

--- client/src/shared/components/Layout.tsx
+++ client/src/shared/components/Layout.tsx
@@ -2,13 +2,19 @@ import * as React from "react";
 import Navbar from "shared/components/Navbar";
 
 interface LayoutProps {}
+
+// Context lets us pass a value deep into the component tree
+// without explicitly threading it through every component.
+// Create a context for the current theme (with "light" as the default).
+const THEME_CONTEXT = React.createContext({});
+
 class Layout extends React.Component<LayoutProps, any> {
   public render(): JSX.Element {
     return (
-      <div>
+      <THEME_CONTEXT.Provider value="light">
         <Navbar />
         {this.props.children}
-      </div>
+      </THEME_CONTEXT.Provider>
     );
   }
 }
