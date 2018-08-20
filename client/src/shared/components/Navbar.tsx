e9ebe8f7036bf8aab729bd377d673d5b060c145e
Nicholas Romero <ncrmro@gmail.com>
2018-08-19 23:37:17 -0500

    fix(client): add initial styles

--- client/src/shared/components/Navbar.tsx
+++ client/src/shared/components/Navbar.tsx
@@ -1,17 +1,24 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 
+import "./Navbar.css";
+
 interface Route {
   name: string;
   href: string;
 }
 
-interface PageProps {
+interface NavbarProps {
+  insertCss?: any;
   routes?: Route[];
 }
 
-class Navbar extends React.Component<PageProps, {}> {
-  public static defaultProps: Partial<PageProps> = {
+class Navbar extends React.Component<NavbarProps, {}> {
+  constructor(props: NavbarProps) {
+    super(props);
+  }
+
+  public static defaultProps: Partial<NavbarProps> = {
     routes: [
       {
         name: "Home",
@@ -44,14 +51,14 @@ class Navbar extends React.Component<PageProps, {}> {
     const { routes } = this.props;
 
     return routes.map((route: Route, index: number) => (
-      <li key={index}>
-        <Link to={route.href}>{route.name}</Link>
-      </li>
+      <Link key={index} to={route.href}>
+        {route.name}
+      </Link>
     ));
   };
 
   public render(): JSX.Element {
-    return <ul>{this.renderRoutes()}</ul>;
+    return <nav>{this.renderRoutes()}</nav>;
   }
 }
 
